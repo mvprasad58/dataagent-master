7b6a931ee3d0b67c13ce8fc6bdb5d51909b23d7e
Vara Prasad <mv@qtx065.qtxad.com>
2018-07-17 12:39:55 +0530

    2nd commit

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+var express  = require('express'),
+bodyParser   = require('body-parser'),
+server       = express();
+
+
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+var routes = require('./API/Routes/Routes'); //importing route
+routes(server); //register the route
+server.listen((process.env.PORT || 8000), function () {
+    console.log("Server is up and listening on port" + process.env.PORT);
+});
